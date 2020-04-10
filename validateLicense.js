eaeff512c93cfdd39122cc861da5e0de0448fdd9
Davidpeche1 <57766463+Davidpeche1@users.noreply.github.com>
2020-04-10 15:07:10 -0400

    Update validateLicense.js

--- validateLicense.js
+++ validateLicense.js
@@ -1,28 +1,13 @@
-const program = require('commander')
-const fs = require('fs')
-
-program
-  .option('-l, --licenses <validLicenses>', 'The list of valid licenses')
-  .parse(process.argv)
-
-async function main() {
-  const event = JSON.parse(fs.readFileSync('/github/workflow/event.json', 'utf8'))
-  const validLicenses = program.licenses.split(',').map(val => val.toLowerCase())
-  let repoLicense
-  if (event.repository.license) repoLicense = event.repository.license.key.toLowerCase()
-
-  if (repoLicense && validLicenses && validLicenses.includes(repoLicense)) return `${repoLicense} is a valid license`
-  throw new Error(`The license ${repoLicense} is not valid in this repo. Allowed licenses are: ${validLicenses}`)
-}
-
-if (require.main === module) {
-  main()
-    .then(res => {
-      console.log({ res })
-      process.exitCode = 0
-    })
-    .catch(err => {
-      console.log({ err })
-      process.exitCode = 1
-    })
+{
+  "name": "validate-license-action",
+  "version": "1.0.0",
+  "lockfileVersion": 1,
+  "requires": true,
+  "dependencies": {
+    "commander": {
+      "version": "2.19.0",
+      "resolved": "https://registry.npmjs.org/commander/-/commander-2.19.0.tgz",
+      "integrity": "sha512-6tvAOO+D6OENvRAh524Dh9jcfKTYDQAqvqezbCW82xj5X0pSrcpxtvRKHLG0yBY6SD7PSDrJaj+0AiOcKVd1Xg=="
+    }
+  }
 }
